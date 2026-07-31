f5061dfd0da3892fbded86859dae7979a2a2811e
Wazhma Hakimi <133529903+WazhmaHakimi@users.noreply.github.com>
2026-07-31 20:31:26 +0430

    Livewire, Preline, and Tailwind added.

---
+++ resources/views/components/⚡home-page.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<div>
+    {{-- Simplicity is the ultimate sophistication. - Leonardo da Vinci --}}
+    <h1 class="text-4xl text-red-500">Hello from livewire 4</h1>
+</div>
