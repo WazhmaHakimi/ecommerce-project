3b31ddf38c23a8992bd92c9e2d5259a98dad9848
Wazhma Hakimi <133529903+WazhmaHakimi@users.noreply.github.com>
2026-08-03 20:12:15 +0430

    Frontend view pages added from https://github.com/DCodeMania/ecommerce-ui-resources Repo, and the routes added.

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::livewire('/', 'home-page');
+Route::view('/', 'pages.home-page')->name('home');
+Route::view('/categories', 'pages.categories-page')->name('categories');
+Route::view('/products', 'pages.products-page')->name('products');
+Route::view('/cart', 'pages.cart-page')->name('cart');
+Route::view('/products/{product}', 'pages.product-detail-page')->name('product-detail');
+Route::view('/checkout', 'pages.checkout-page')->name('checkout');
+Route::view('/my-orders', 'pages.my-orders-page')->name('my-orders');
+Route::view('/my-orders/{order}', 'pages.order-detail-page')->name('my-order-detail');
+Route::view('/login', 'pages.auth.login-page')->name('login');
+Route::view('/register', 'pages.auth.register-page')->name('register');
+Route::view('/reset', 'pages.auth.reset-password-page')->name('reset-password');
+Route::view('/forgot', 'pages.auth.forgot-page')->name('forgot-password');
+Route::view('/success', 'pages.success-page')->name('success');
+Route::view('/cancel', 'pages.cancel-page')->name('cancel');
