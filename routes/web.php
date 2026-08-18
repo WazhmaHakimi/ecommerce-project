76398b6bced2e7ab4d63ccb04fbf5eead5e5a3c4
Wazhma Hakimi <133529903+WazhmaHakimi@users.noreply.github.com>
2026-08-18 19:44:56 +0430

    Pages and routes corrected.

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'pages.home-page')->name('home');
-Route::view('/categories', 'pages.categories-page')->name('categories');
-Route::view('/products', 'pages.products-page')->name('products');
-Route::view('/cart', 'pages.cart-page')->name('cart');
-Route::view('/products/{product}', 'pages.product-detail-page')->name('product-detail');
-Route::view('/checkout', 'pages.checkout-page')->name('checkout');
-Route::view('/my-orders', 'pages.my-orders-page')->name('my-orders');
-Route::view('/my-orders/{order}', 'pages.order-detail-page')->name('my-order-detail');
-Route::view('/login', 'pages.auth.login-page')->name('login');
-Route::view('/register', 'pages.auth.register-page')->name('register');
-Route::view('/reset', 'pages.auth.reset-password-page')->name('reset-password');
-Route::view('/forgot', 'pages.auth.forgot-page')->name('forgot-password');
-Route::view('/success', 'pages.success-page')->name('success');
-Route::view('/cancel', 'pages.cancel-page')->name('cancel');
+Route::livewire('/', 'pages::home-page')->name('home');
+Route::livewire('/categories', 'pages::categories-page')->name('categories');
+Route::livewire('/products', 'pages::products-page')->name('products');
+Route::livewire('/cart', 'pages::cart-page')->name('cart');
+Route::livewire('/products/{product}', 'pages::product-detail-page')->name('product-detail');
+Route::livewire('/checkout', 'pages::checkout-page')->name('checkout');
+Route::livewire('/my-orders', 'pages::my-orders-page')->name('my-orders');
+Route::livewire('/my-orders/{order}', 'pages::order-detail-page')->name('my-order-detail');
+Route::livewire('/login', 'pages::auth.login-page')->name('login');
+Route::livewire('/register', 'pages::auth.register-page')->name('register');
+Route::livewire('/reset', 'pages::auth.reset-password-page')->name('reset-password');
+Route::livewire('/forgot', 'pages::auth.forgot-page')->name('forgot-password');
+Route::livewire('/success', 'pages::success-page')->name('success');
+Route::livewire('/cancel', 'pages::cancel-page')->name('cancel');
