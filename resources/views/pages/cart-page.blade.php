87d19f0836149d48a9a20f57e41f10e7b9835a4b
Wazhma Hakimi <133529903+WazhmaHakimi@users.noreply.github.com>
2026-08-27 15:49:58 +0430

    Remove button functionality added to cart page.

--- resources/views/pages/cart-page.blade.php
+++ resources/views/pages/cart-page.blade.php
@@ -1,7 +1,7 @@
 <?php
 
 use Livewire\Component;
-use Livewire\Attributes\Title; 
+use Livewire\Attributes\Title;
 use App\Helpers\CartManagement;
 
 new #[Title('Cart | E-Commerce')] class extends Component {
@@ -12,6 +12,25 @@ new #[Title('Cart | E-Commerce')] class extends Component {
     {
         $this->cart_items = CartManagement::getCartItemsFromCookies();
 
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
+    public function removeItem($product_id)
+    {
+        $this->cart_items = CartManagement::removeCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+        $this->dispatch('update-cart-count', total_count: count($this->cart_items));
+    }
+
+    public function incrementQuantity($product_id)
+    {
+        $this->cart_items = CartManagement::incrementQuantityOfCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
+    public function decrementQuantity($product_id)
+    {
+        $this->cart_items = CartManagement::decrementQuantityOfCartItem($product_id);
         $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
     }
 };
@@ -35,34 +54,41 @@ new #[Title('Cart | E-Commerce')] class extends Component {
                         </thead>
                         <tbody>
                             @forelse ($this->cart_items as $item)
-                            <tr wire:key="{{ $item['product_id'] }}">
-                                <td class="py-4">
-                                    <div class="flex items-center">
-                                        <img class="h-16 w-16 mr-4" src="{{ url('storage', $item['image']) }}"
-                                            alt="{{ $item['name'] }}">
-                                        <span class="font-semibold">{{ $item['name'] }}</span>
-                                    </div>
-                                </td>
-                                <td class="py-4">{{ Number::currency($item['unit_amount'], 'AFN') }}</td>
-                                <td class="py-4">
-                                    <div class="flex items-center">
-                                        <button class="border rounded-md py-2 px-4 mr-2">-</button>
-                                        <span class="text-center w-8">{{ $item['quantity'] }}</span>
-                                        <button class="border rounded-md py-2 px-4 ml-2">+</button>
-                                    </div>
-                                </td>
-                                <td class="py-4">{{ Number::currency($item['total_amount'], 'AFN') }}</td>
-                                <td><button
-                                        class="bg-slate-300 border-2 border-slate-400 rounded-lg px-3 py-1 hover:bg-red-500 hover:text-white hover:border-red-700">Remove</button>
-                                </td>
-                            </tr>
+                                <tr wire:key="{{ $item['product_id'] }}">
+                                    <td class="py-4">
+                                        <div class="flex items-center">
+                                            <img class="h-16 w-16 mr-4" src="{{ url('storage', $item['image']) }}"
+                                                alt="{{ $item['name'] }}">
+                                            <span class="font-semibold">{{ $item['name'] }}</span>
+                                        </div>
+                                    </td>
+                                    <td class="py-4">{{ Number::currency($item['unit_amount'], 'AFN') }}</td>
+                                    <td class="py-4">
+                                        <div class="flex items-center">
+                                            <button wire:click='decrementQuantity({{ $item['product_id'] }})'
+                                                class="border rounded-md py-2 px-4 mr-2">-</button>
+                                            <span class="text-center w-8">{{ $item['quantity'] }}</span>
+                                            <button wire:click='incrementQuantity({{ $item['product_id'] }})'
+                                                class="border rounded-md py-2 px-4 ml-2">+</button>
+                                        </div>
+                                    </td>
+                                    <td class="py-4">{{ Number::currency($item['total_amount'], 'AFN') }}</td>
+                                    <td><button wire:click='removeItem({{ $item['product_id'] }})'
+                                            class="bg-slate-300 border-2 border-slate-400 rounded-lg px-3 py-1 hover:bg-red-500 hover:text-white hover:border-red-700">
+                                            <span wire:loading.remove
+                                                wire:target='removeItem({{ $item['product_id'] }})'>Remove</span><span
+                                                wire:loading
+                                                wire:target='removeItem({{ $item['product_id'] }})'>Removing...</span>
+                                        </button>
+                                    </td>
+                                </tr>
                             @empty
 
-                            <tr>
-                                <td colspan="5" class="text-center py-4 text-4xl font-semibold  text-slate-500">No Items
-                                    available in the cart!</td>
-                            </tr>
-
+                                <tr>
+                                    <td colspan="5" class="text-center py-4 text-4xl font-semibold  text-slate-500">
+                                        No Items
+                                        available in the cart!</td>
+                                </tr>
                             @endforelse
 
                         </tbody>
@@ -91,7 +117,7 @@ new #[Title('Cart | E-Commerce')] class extends Component {
                     </div>
 
                     @if ($cart_items)
-                    <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+                        <button class="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
                     @endif
                 </div>
             </div>
