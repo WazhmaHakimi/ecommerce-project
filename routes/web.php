2162e2059296153948e4757cd36d854ee1a0a01b
Wazhma Hakimi <133529903+WazhmaHakimi@users.noreply.github.com>
2026-08-28 16:38:13 +0430

    Authentication functionality added to login and registeration, and navbar appearance updated based on it.

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,23 @@ Route::livewire('/categories', 'pages::categories-page')->name('categories');
 Route::livewire('/products', 'pages::products-page')->name('products');
 Route::livewire('/cart', 'pages::cart-page')->name('cart');
 Route::livewire('/products/{slug}', 'pages::product-detail-page')->name('product-detail');
-Route::livewire('/checkout', 'pages::checkout-page')->name('checkout');
-Route::livewire('/my-orders', 'pages::my-orders-page')->name('my-orders');
-Route::livewire('/my-orders/{order}', 'pages::order-detail-page')->name('my-order-detail');
-Route::livewire('/login', 'pages::auth.login-page')->name('login');
-Route::livewire('/register', 'pages::auth.register-page')->name('register');
-Route::livewire('/reset', 'pages::auth.reset-password-page')->name('reset-password');
-Route::livewire('/forgot', 'pages::auth.forgot-page')->name('forgot-password');
-Route::livewire('/success', 'pages::success-page')->name('success');
-Route::livewire('/cancel', 'pages::cancel-page')->name('cancel');
+
+
+Route::middleware('guest')->group(function () {
+    Route::livewire('/login', 'pages::auth.login-page')->name('login');
+    Route::livewire('/register', 'pages::auth.register-page')->name('register');
+    Route::livewire('/reset', 'pages::auth.reset-password-page')->name('reset-password');
+    Route::livewire('/forgot', 'pages::auth.forgot-page')->name('forgot-password');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', function () {
+        auth()->logout();
+        return redirect('/');
+    });
+    Route::livewire('/checkout', 'pages::checkout-page')->name('checkout');
+    Route::livewire('/my-orders', 'pages::my-orders-page')->name('my-orders');
+    Route::livewire('/my-orders/{order}', 'pages::order-detail-page')->name('my-order-detail');
+    Route::livewire('/success', 'pages::success-page')->name('success');
+    Route::livewire('/cancel', 'pages::cancel-page')->name('cancel');
+});
